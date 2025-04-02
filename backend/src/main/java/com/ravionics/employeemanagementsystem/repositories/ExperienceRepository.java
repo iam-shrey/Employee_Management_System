2055d35cbcbf49ae144603f45177fcd6b997ceff
Shreyansh Jain <145953907+iam-shrey@users.noreply.github.com>
2025-04-02 23:04:55 +0530

    Delete backend directory

--- backend/src/main/java/com/ravionics/employeemanagementsystem/repositories/ExperienceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ravionics.employeemanagementsystem.repositories;
-
-import com.ravionics.employeemanagementsystem.entities.Experience;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExperienceRepository extends JpaRepository<Experience, Long> {
-}
--- backend/src/main/java/com/ravionics/employeemanagementsystem/repositories/OfferTemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ravionics.employeemanagementsystem.repositories;
-
-import com.ravionics.employeemanagementsystem.entities.OfferTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfferTemplateRepository extends JpaRepository<OfferTemplate, Integer> {
-}
